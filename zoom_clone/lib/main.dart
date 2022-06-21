bf684e47f58990b75796a8cbf2447e3e6d03df77
yjchang-april7 <yjchang@april7.co.kr>
2022-06-21 16:33:36 +0900

    화상 대화방 참여 기능. 방 정보 기록.

--- zoom_clone/lib/main.dart
+++ zoom_clone/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:zoom_clone/resources/auth_methods.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
+import 'package:zoom_clone/screens/video_call_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => const LoginScreen(),
         '/home': (context) => const HomeScreen(),
+        '/video-call': ((context) => const VideoCallScreen()),
       },
       home: StreamBuilder(
         stream: AuthMethods().authChanges,
---
+++ zoom_clone/lib/resources/jitsi_meet_methods.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:jitsi_meet/feature_flag/feature_flag.dart';
+import 'package:jitsi_meet/jitsi_meet.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
+import 'package:zoom_clone/resources/firestore_methods.dart';
+
+class JitsiMeetMethods {
+  final AuthMethods _authMethods = AuthMethods();
+  final FirestoreMethods _firestoreMethods = FirestoreMethods();
+
+  void createNewMeeting({
+    required String roomName,
+    required bool isAudioMuted,
+    required bool isVideoMuted,
+    String username = '',
+  }) async {
+    try {
+      // FeatureFlag featureFlag = FeatureFlag();
+      // featureFlag.welcomePageEnabled = false;
+      // featureFlag.resolution = FeatureFlagVideoResolution.MD_RESOLUTION;
+      Map<FeatureFlagEnum, bool> featureFlags = {};
+      featureFlags[FeatureFlagEnum.WELCOME_PAGE_ENABLED] = false;
+      if (Platform.isAndroid) {
+        // Disable ConnectionService usage on Android to avoid issues (see README)
+        featureFlags[FeatureFlagEnum.CALL_INTEGRATION_ENABLED] = false;
+      } else if (Platform.isIOS) {
+        // Disable PIP on iOS as it looks weird
+        featureFlags[FeatureFlagEnum.PIP_ENABLED] = false;
+      }
+
+      String name;
+      if (username.isEmpty) {
+        name = _authMethods.user.displayName!;
+      } else {
+        name = username;
+      }
+      var options = JitsiMeetingOptions(room: roomName)
+        ..userDisplayName = name
+        ..userEmail = _authMethods.user.email
+        ..userAvatarURL = _authMethods.user.photoURL
+        ..audioMuted = isAudioMuted
+        ..videoMuted = isVideoMuted
+        ..featureFlags.addAll(featureFlags);
+
+      _firestoreMethods.addToMeetingHistory(roomName);
+      await JitsiMeet.joinMeeting(options);
+    } catch (error) {
+      print("error: $error");
+    }
+  }
+}
