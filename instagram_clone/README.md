81dafe560b687bd9e58c6646909d5bacdc9f36f9
yjchang-april7 <yjchang@april7.co.kr>
2022-06-22 17:23:39 +0900

    Instagram Clone

---
+++ instagram_clone/README.md
@@ -0,0 +1,14 @@
+# instagram_clone
+
+A new Flutter project.
+
+## Getting Started
+https://www.youtube.com/watch?v=mEPm9w5QlJM
+https://github.com/RivaanRanawat/instagram-flutter-clone
+
+## 시뮬레이터 실행
+$ open -a simulator
+
+## 웹 환경 실행
+$ flutter run -d chrome --web-renderer html 
+
---
+++ instagram_clone/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/firebase_options.dart';
+import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
+import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDiqJA6CqR4T57gW-bOTMDE2zSYPwbVfhk",
+        appId: "1:886662903727:web:70db73141e1e9530fe615a",
+        messagingSenderId: "886662903727",
+        projectId: "instagram-clone-3bf3a",
+        storageBucket: "instagram-clone-3bf3a.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      // home: const ResponsiveLayout(
+      //   webScreenLayout: WebScreenLayout(),
+      //   mobileScreenLayout: MobileScreenLayout(),
+      // ),
+      home: LoginScreen(),
+    );
+  }
+}
