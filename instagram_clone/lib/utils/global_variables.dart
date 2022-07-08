778e9b8b814fb9ddb98086059fd6cb7131a77178
yjchang-april7 <yjchang@april7.co.kr>
2022-07-08 17:23:03 +0900

    포스트 피드 페이지 작업

--- instagram_clone/lib/utils/global_variables.dart
+++ instagram_clone/lib/utils/global_variables.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/feed_screen.dart';
 
 import '../screens/add_post_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Center(
-    child: Text('feed'),
-  ),
+  const FeedScreen(),
   Center(
     child: Text('search'),
   ),
