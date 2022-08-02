22ff34e40d642d03f706a9b1e66ca86de951d074
yjchang-april7 <yjchang@april7.co.kr>
2022-08-02 17:24:42 +0900

    Whatsapp 초기 작업, 유투브에 나와 있지 않은 초기 코드 작업. https://github.com/RivaanRanawat/whatsapp-flutter-ui

---
+++ whatsapp-clone/lib/widgets/web_profile_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:whatsapp_ui/colors.dart';
+
+class WebProfileBar extends StatelessWidget {
+  const WebProfileBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.077,
+      width: MediaQuery.of(context).size.width * 0.25,
+      padding: const EdgeInsets.all(10.0),
+      decoration: const BoxDecoration(
+          border: Border(
+            right: BorderSide(
+              color: dividerColor,
+            ),
+          ),
+          color: webAppBarColor),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+              'ttps://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+            ),
+            radius: 20,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.comment,
+                  color: Colors.grey,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_vert,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
